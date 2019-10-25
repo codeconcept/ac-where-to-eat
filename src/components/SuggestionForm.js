9ff9b5eda4b1c6dbd31e2fd564e5550c15c1c269
codeconcept <contact@codeconcept.fr>
2019-10-25 11:53:23 +0200

    First class component

---
+++ src/components/SuggestionForm.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class SuggestionForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      places: []
+    }
+  }
+  render() { 
+    return (
+      <div>Form works!</div>
+    );
+  }
+}
+ 
+export default SuggestionForm;
