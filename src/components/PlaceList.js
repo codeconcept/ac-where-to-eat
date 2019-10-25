7ba0815d4afa1b8b55cd9448852bf2543523f746
codeconcept <contact@codeconcept.fr>
2019-10-25 14:06:22 +0200

    rendering the props passed to an SFC

--- src/components/PlaceList.js
+++ src/components/PlaceList.js
@@ -2,7 +2,13 @@ import React from "react";
 
 const PlaceList = props => {
   console.log(props);
-  return <div>PlaceList works!</div>;
+  return (
+    <div>
+      {props.places.map(p => (
+        <div key={p.id}>{p.name}</div>
+      ))}
+    </div>
+  );
 };
 
 export default PlaceList;
