ad23f9b5fe7b36bb92a76314ec157637d8b92d84
codeconcept <contact@codeconcept.fr>
2019-10-25 12:27:29 +0200

    Resetting form

--- src/components/SuggestionForm.js
+++ src/components/SuggestionForm.js
@@ -17,6 +17,7 @@ class SuggestionForm extends React.Component {
   handleSubmit(evt) {
     evt.preventDefault();
     this.props.onPlaceSubmit(this.state.place);
+    this.setState({ place: "" });
   }
   render() {
     return (
