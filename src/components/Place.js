65199069c5f3ee789af33da926706abeb423e096
codeconcept <contact@codeconcept.fr>
2019-10-25 14:16:55 +0200

    Object destructuring on props

--- src/components/Place.js
+++ src/components/Place.js
@@ -1,7 +1,7 @@
 import React from "react";
 
-const Place = props => {
-  return <div>{props.data.name}</div>;
+const Place = ({ data }) => {
+  return <div>{data.name}</div>;
 };
 
 export default Place;
--- src/components/PlaceList.js
+++ src/components/PlaceList.js
@@ -2,9 +2,10 @@ import React from "react";
 import Place from "./Place";
 
 const PlaceList = props => {
+  const { places } = props;
   return (
     <div>
-      {props.places.map(p => (
+      {places.map(p => (
         <Place data={p} key={p.id} />
       ))}
     </div>
